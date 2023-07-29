e819ae311557ff1e744d05608664c974f9bf908c
wasp <margi4@op.pl>
2023-07-29 18:53:32 +0200

    Add code

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector("#menu-btn");
+const facebookBtn = document.querySelector("#facebook-btn");
+const sendEmailBtn = document.querySelector("#send-email-btn");
+
+function toggleMenu() {
+  navbar.classList.toggle("active");
+}
+
+menuBtn.addEventListener("click", toggleMenu);
+window.addEventListener("scroll", navbar.classList.remove("active"));
+
